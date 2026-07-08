255ef70a9e445d67c5a93101cce0cdb4d12535bb
kudinDmitriyUp <kudin.dmitriy.up@gmail.com>
2026-07-08 02:44:26 +0000

    Bob AI: Update the staff photo captions in the team section: set the first to 'David', t

--- src/pages/HomePage/sections/Team.tsx
+++ src/pages/HomePage/sections/Team.tsx
@@ -14,9 +14,9 @@ export default function TeamSection(): React.JSX.Element {
           title="Meet Our Management Team"
           description="Dedicated professionals committed to event success."
           members={[
-            { name: "Elena Vance", role: "Director", imageSrc: "https://storage.googleapis.com/webild/users/user_3GCXkgaOrxONyQE8SfG9tbExgNj/uploaded-1783478473070-shm5mxkp.png" },
-            { name: "Marcus Thorne", role: "Ops Lead", imageSrc: "https://storage.googleapis.com/webild/users/user_3GCXkgaOrxONyQE8SfG9tbExgNj/uploaded-1783478473073-qq9abrct.png" },
-            { name: "Sarah Jenkins", role: "Recruiting", imageSrc: "https://storage.googleapis.com/webild/users/user_3GCXkgaOrxONyQE8SfG9tbExgNj/uploaded-1783478549327-e2031yx1.png" },
+            { name: "David", role: "Server", imageSrc: "https://storage.googleapis.com/webild/users/user_3GCXkgaOrxONyQE8SfG9tbExgNj/uploaded-1783478473070-shm5mxkp.png" },
+            { name: "Josue", role: "Server", imageSrc: "https://storage.googleapis.com/webild/users/user_3GCXkgaOrxONyQE8SfG9tbExgNj/uploaded-1783478473073-qq9abrct.png" },
+            { name: "Seif", role: "Server", imageSrc: "https://storage.googleapis.com/webild/users/user_3GCXkgaOrxONyQE8SfG9tbExgNj/uploaded-1783478549327-e2031yx1.png" },
           ]}
           textAnimation="slide-up"
         />
